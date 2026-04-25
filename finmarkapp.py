52ca42ccf6fa20f3b94518b00365d2c61f30c012
Bongumusa Joseph Hlongwane <94066555+Joemusa@users.noreply.github.com>
2026-04-25 11:02:32 +0200

    Update finmarkapp.py

--- finmarkapp.py
+++ finmarkapp.py
@@ -4,3 +4,9 @@ import pandas as pd
 st.set_page_config(layout="wide")
 
 st.title("📊 Financial Dashboard")
+
+@st.cache_data
+def load_data():
+    return pd.read_csv("clean_data.csv")
+
+df = load_data()
