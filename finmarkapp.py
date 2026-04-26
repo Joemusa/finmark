3ec7046fe8892f04260efc191fdc3d5775c3c2ed
Bongumusa Joseph Hlongwane <94066555+Joemusa@users.noreply.github.com>
2026-04-26 05:09:55 +0200

    Update finmarkapp.py

--- finmarkapp.py
+++ finmarkapp.py
@@ -6,6 +6,7 @@ import plotly.express as px
 # PAGE SETUP
 # -----------------------
 st.set_page_config(layout="wide")
+st.image("logo.png", width=200)
 st.title("📊 Financial Inclusion Dashboard")
 
 # -----------------------
